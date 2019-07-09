2669a952445b097a287a53932cbd6f1ba82e7fa4
Michael Staton <mpstaton@gmail.com>
2019-07-09 10:01:34 -0700

    minor change now makes blog post lookup dynamic based on the ID at the end of the route

--- src/blog/views.py
+++ src/blog/views.py
@@ -4,10 +4,8 @@ from django.shortcuts import render
 
 from .models import BlogPost
 
-obj = BlogPost.objects.get(id=1)
-
-def blog_post_detail_page(request):
-
+def blog_post_detail_page(request, post_id):
+    obj = BlogPost.objects.get(id=post_id)
     template_name = 'blog_post_detail.html'
     context = {"object": obj}
     return render(request, template_name, context)
--- src/try_django/urls.py
+++ src/try_django/urls.py
@@ -29,7 +29,7 @@ from .views import (
 
 urlpatterns = [
     path('', home_page),
-    path('blog/', blog_post_detail_page),
+    path('blog/<int:post_id>/', blog_post_detail_page),
     path('about/', about_page),
     path('contact/', contact_page),
     path('example/', example_page),
