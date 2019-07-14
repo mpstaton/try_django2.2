698fcbbd159fa374b1e75a7e1209a2f1c3b61379
Michael Staton <mpstaton@gmail.com>
2019-07-14 14:58:00 -0700

    implemented Django's login required

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,3 +1,6 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib.admin.views.decorators import staff_member_required
+from django.http import Http404
 from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
@@ -18,6 +21,8 @@ def blog_post_list_view(request):
     context = {'object_list': qs }
     return render(request, template_name, context)
 
+@staff_member_required
+@login_required
 def blog_post_create_view(request):
     #obj = get_object_or_404(BlogPost, slug=slug)
     form = BlogPostModelForm(request.POST or None)
