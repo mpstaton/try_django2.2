766298e97bc6431d05381ef2e7fded7c856c4321
Michael Staton <mpstaton@gmail.com>
2019-07-14 14:03:47 -0700

    refactored the Contact form to use DjangoForms

--- src/try_django/views.py
+++ src/try_django/views.py
@@ -2,6 +2,8 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.template.loader import get_template
 
+from .forms import ContactForm
+
 def home_page(request):
     my_title = "ET Phone Home"
     context = {"title": "Who will phone home?"}
@@ -13,8 +15,15 @@ def about_page(request):
     return render(request, "about.html", {"title": "About"})
 
 def contact_page(request):
-    print(request.POST)
-    return render(request, "form.html", {"title": "Contact"})
+    form = ContactForm(request.POST or None)
+    if form.is_valid():
+        print(form.cleaned_data)
+        form = ContactForm()
+    context = {
+        "title": "Contact Us",
+        "form": form
+    }       
+    return render(request, "form.html", {"title": "Contact", "form": form})
 
 def example_page(request):
     context = {"title": "Example"}
